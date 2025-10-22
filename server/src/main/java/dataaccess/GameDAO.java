f11bd51d56ff850ba3d2171e53c6b0c9be73d22b
21dbly <21dbly@gmail.com>
2025-10-22 00:52:41 -0600

    added DAO(Data Access Object) interfaces

---
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -0,0 +1,13 @@
+package dataaccess;
+
+import model.GameData;
+
+import java.util.List;
+
+public interface GameDAO {
+    void clear() throws DataAccessException;
+    void createGame(GameData gameData) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    List<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData gameData) throws DataAccessException;
+}
