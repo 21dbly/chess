850bf5ef7cec559753ebdf6012e3f83632b13ed8
21dbly <21dbly@gmail.com>
2025-10-22 20:07:46 -0600

    added clear server endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,14 +1,23 @@
 package server;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
 import io.javalin.*;
+import io.javalin.http.Context;
+import service.ChessService;
 
 public class Server {
 
     private final Javalin javalin;
+    private final ChessService service;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        this.service = new ChessService(new MemoryUserDAO(), new MemoryAuthDAO(), new MemoryGameDAO());
 
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                .delete("/db", this::clear);
         // Register your endpoints and exception handlers here.
 
     }
@@ -20,5 +29,9 @@ public class Server {
 
     public void stop() {
         javalin.stop();
+    }
+
+    private void clear(Context ctx) throws DataAccessException {
+        service.clear();
     }
 }
