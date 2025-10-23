8430e511f56ddb3ac9e72346565b83f326b37a4b
21dbly <21dbly@gmail.com>
2025-10-22 19:33:58 -0600

    made login tests

--- server/src/test/java/service/ChessServiceTests.java
+++ server/src/test/java/service/ChessServiceTests.java
@@ -11,10 +11,12 @@ public class ChessServiceTests {
             new MemoryUserDAO(), new MemoryAuthDAO(), new MemoryGameDAO()
     );
 
-    final UserData user1 = new UserData("userOne", "pass123", "one@mail.com");
-    final LoginRequest user1Login = new LoginRequest("userOne", "pass123");
+    final UserData user1 = new UserData("userOne", "pass111", "one@mail.com");
+    final LoginRequest user1Login = new LoginRequest("userOne", "pass111");
     final LoginRequest wrongPasswordLogin = new LoginRequest("userOne", "wrongPass");
     final UserData testSameUsername = new UserData("userOne", "differentPass", "different@mail.com");
+    final UserData user2 = new UserData("userTwo", "pass222", "two@mail.com");
+    final LoginRequest user2Login = new LoginRequest("userTwo", "pass222");
 
     @Test
     void registerValid() throws RegistrationException, DataAccessException {
@@ -30,5 +32,30 @@ public class ChessServiceTests {
         service.register(user1);
         assertThrows(RegistrationException.class, () ->
                 service.register(testSameUsername));
+    }
+
+    @Test
+    void loginValid() throws RegistrationException, DataAccessException {
+        service.clear();
+        service.register(user1);
+        AuthData authData = service.login(user1Login);
+        assertSame(authData.username(), user1.username());
+        assertNotNull(authData.authToken());
+    }
+
+    @Test
+    void loginWrongPassword() throws RegistrationException, DataAccessException {
+        service.clear();
+        service.register(user1);
+        assertThrows(Exception.class, () ->
+                service.login(wrongPasswordLogin));
+    }
+
+    @Test
+    void loginWrongUsername() throws RegistrationException, DataAccessException {
+        service.clear();
+        service.register(user1);
+        assertThrows(Exception.class, () ->
+                service.login(user2Login));
     }
 }
