393f1cd6ac6a574ac8967a054a3454f61f162cb1
21dbly <21dbly@gmail.com>
2025-11-03 22:09:27 -0700

    made createAuth tests

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -15,6 +15,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DataAccessTests {
 
+    final AuthData user1Auth = new AuthData("12345", "userOne");
+    final AuthData authNoUser = new AuthData("12345", null);
+    final AuthData authNoToken = new AuthData(null, "userOne");
+    final LoginRequest user1Login = new LoginRequest("userOne", "pass111");
+    final LoginRequest wrongPasswordLogin = new LoginRequest("userOne", "wrongPass");
+    final UserData testSameUsername = new UserData("userOne", "differentPass", "different@mail.com");
+    final UserData user2 = new UserData("userTwo", "pass222", "two@mail.com");
+    final LoginRequest user2Login = new LoginRequest("userTwo", "pass222");
+    final String game1Name = "game1!";
+    final String game2Name = "game2?";
+
     private AuthDAO getAuthDAO(Class<? extends AuthDAO> authDAOClass) throws DataAccessException {
         AuthDAO authDAO;
         if (authDAOClass.equals(SQLAuthDAO.class)) {
@@ -28,7 +39,24 @@ class DataAccessTests {
 
     @ParameterizedTest
     @ValueSource(classes = {SQLAuthDAO.class, MemoryAuthDAO.class})
-    void testTest(Class<? extends AuthDAO> authDAOclass) throws ResponseException {
+    void createAuthValid(Class<? extends AuthDAO> authDAOclass) throws ResponseException {
         AuthDAO authDAO = getAuthDAO(authDAOclass);
+        assertDoesNotThrow(() -> authDAO.createAuth(user1Auth));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {SQLAuthDAO.class, MemoryAuthDAO.class})
+    void createAuthNoToken(Class<? extends AuthDAO> authDAOclass) throws ResponseException {
+        AuthDAO authDAO = getAuthDAO(authDAOclass);
+        assertThrows(DataAccessException.class, () ->
+                authDAO.createAuth(authNoToken));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {SQLAuthDAO.class, MemoryAuthDAO.class})
+    void createAuthNoUsername(Class<? extends AuthDAO> authDAOclass) throws ResponseException {
+        AuthDAO authDAO = getAuthDAO(authDAOclass);
+        assertThrows(DataAccessException.class, () ->
+                authDAO.createAuth(authNoUser));
     }
 }
