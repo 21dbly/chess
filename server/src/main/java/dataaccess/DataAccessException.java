95a89fb4c60d4a24390e46a03ace3f7fdfb22c25
21dbly <21dbly@gmail.com>
2025-11-04 02:56:18 -0700

    passing all tests

--- server/src/main/java/dataaccess/DataAccessException.java
+++ server/src/main/java/dataaccess/DataAccessException.java
@@ -8,9 +8,9 @@ import exceptions.ResponseException;
 public class DataAccessException extends ResponseException {
     // is it a bad idea to modify this?
     public DataAccessException(String message) {
-        super(500, message);
+        super(500, "Error: "+message);
     }
     public DataAccessException(String message, Throwable ex) {
-        super(500, message, ex);
+        super(500, "Error: "+message, ex);
     }
 }
