70f1bc8915f89a17a806084758383d5f76d9e7f4
21dbly <21dbly@gmail.com>
2025-11-14 15:07:38 -0700

    board printing fully

--- client/src/main/java/ui/BoardPrinter.java
+++ client/src/main/java/ui/BoardPrinter.java
@@ -1,11 +1,19 @@
 package ui;
 
 import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
 import static ui.EscapeSequences.*;
 
 public class BoardPrinter {
 
     private static final String BORDER_STYLE = SET_BG_COLOR_LIGHT_GREY + SET_TEXT_COLOR_BLACK;
+    private static final String WHITE_BACKGROUND = SET_BG_COLOR_WHITE;
+    private static final String BLACK_BACKGROUND = SET_BG_COLOR_BLACK;
+    private static final String WHITE_PIECE = SET_TEXT_COLOR_RED;
+    private static final String BLACK_PIECE = SET_TEXT_COLOR_BLUE;
 
     public static String BoardPrintString(ChessBoard board, String color) {
         StringBuilder retString = new StringBuilder();
@@ -21,18 +29,18 @@ public class BoardPrinter {
 
         for (int r = startRow; r >= 0 && r <= 9; r += incrementRow) {
             for (int c = startCol; c >= 0 && c <= 9; c += incrementCol) {
-                retString.append(getStringFromCoords(r, c));
+                retString.append(getStringFromCoords(r, c, board));
             }
             retString.append(RESET_TEXT+"\n");
         }
         return retString.toString();
     }
 
-    private static String getStringFromCoords(int r, int c) {
+    private static String getStringFromCoords(int r, int c, ChessBoard board) {
         if (r == 0 || r == 9 || c == 0 || c == 9) {
             return getBorderText(r, c);
         } else {
-            return RESET_TEXT + "   ";
+            return getSquareText(r, c, board);
         }
     }
 
@@ -40,7 +48,19 @@ public class BoardPrinter {
         if (r == 0 || r == 9) {
             return BORDER_STYLE + " "+numToLetter(c)+" ";
         } else {
-            return BORDER_STYLE + " "+String.valueOf(r)+" ";
+            return BORDER_STYLE + " "+ r +" ";
+        }
+    }
+
+    private static String getSquareText(int r, int c, ChessBoard board) {
+        String background = ((r + c) % 2 == 1) ? WHITE_BACKGROUND : BLACK_BACKGROUND;
+        ChessPiece piece = board.getPiece(new ChessPosition(r, c));
+        if (piece == null) {
+            return background + "   ";
+        } else if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+            return background + WHITE_PIECE + " "+piece.toString().toUpperCase()+" ";
+        } else {
+            return background + BLACK_PIECE + " "+piece.toString().toUpperCase()+" ";
         }
     }
 
