9d68572962d4b9ce33760b6bc2c7cd431b709295
21dbly <21dbly@gmail.com>
2025-11-14 01:01:53 -0700

    copied serverFacade helper functions from pet shop

--- client/src/main/java/serverfacade/ServerFacade.java
+++ client/src/main/java/serverfacade/ServerFacade.java
@@ -1,8 +1,13 @@
 package serverfacade;
 
+import com.google.gson.Gson;
+import exceptions.ResponseException;
 import model.*;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Collection;
 
 public class ServerFacade {
@@ -39,6 +44,54 @@ public class ServerFacade {
 
     public void joinGame(String authToken, String playerColor, int gameID) {
         throw new RuntimeException("Not implemented");
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private HttpRequest.BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return HttpRequest.BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
+        try {
+            return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw ResponseException.fromJson(body);
+            }
+
+            throw new ResponseException(status, "other failure: " + status);
+        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
     }
 
 }
--- shared/src/main/java/exceptions/ResponseException.java
+++ shared/src/main/java/exceptions/ResponseException.java
@@ -2,6 +2,7 @@ package exceptions;
 
 import com.google.gson.Gson;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ResponseException extends Exception {
@@ -19,6 +20,13 @@ public class ResponseException extends Exception {
 
     public String toJson() {
         return new Gson().toJson(Map.of("message", getMessage(), "status", code));
+    }
+
+    public static ResponseException fromJson(String json) {
+        var map = new Gson().fromJson(json, HashMap.class);
+        int status = Integer.parseInt(map.get("status").toString());
+        String message = map.get("message").toString();
+        return new ResponseException(status, message);
     }
 
     public int code() {
