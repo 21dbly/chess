3da3f05c4a5260dd7b7965325fe86fb855da2ee4
21dbly <21dbly@gmail.com>
2025-12-04 00:52:59 -0700

    server websocket connect partly functioning

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -1,22 +1,60 @@
 package websocket;
 
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.SQLGameDAO;
 import io.javalin.websocket.*;
-import org.jetbrains.annotations.NotNull;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.UserGameCommand;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+    GameDAO gameDAO;
+
+    public WebSocketHandler() {
+        try {
+            gameDAO = new SQLGameDAO();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
-    public void handleConnect(@NotNull WsConnectContext wsConnectContext) throws Exception {
+    public void handleConnect(WsConnectContext wsConnectContext) throws Exception {
         System.out.println("Websocket connected");
         wsConnectContext.enableAutomaticPings();
     }
 
     @Override
-    public void handleClose(@NotNull WsCloseContext wsCloseContext) throws Exception {
+    public void handleClose(WsCloseContext wsCloseContext) throws Exception {
         System.out.println("Websocket closed");
     }
 
     @Override
-    public void handleMessage(@NotNull WsMessageContext wsMessageContext) throws Exception {
-        System.out.println("Message received: "+wsMessageContext.message());
+    public void handleMessage(WsMessageContext ctx) throws Exception {
+        System.out.println("Message received: "+ctx.message());
+        try {
+            UserGameCommand command = new Gson().fromJson(ctx.message(), UserGameCommand.class);
+            switch (command.getCommandType()) {
+                case CONNECT -> connect(command.getGameID(), command.getAuthToken(), ctx.session);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void connect(int gameID, String authToken, Session session) throws IOException, DataAccessException {
+        var game = gameDAO.getGame(gameID);
+        if (game == null) {
+            return;
+        }
+        // add to connectionManager using gameID and session
+        String message = new LoadGameMessage(game.game().getBoard()).toString();
+        session.getRemote().sendString(message);
+        // broadcast to others
     }
 }
